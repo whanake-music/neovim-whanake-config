d8c30c34240ce8466c215a7bba2ef2a281de9960
whanake-music <whanake@tutanota.com>
2023-03-20 11:32:09 +1300

    Add autocompletion and snippets

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -18,6 +18,14 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-lualine/lualine.nvim'
   use 'nvim-treesitter/nvim-treesitter'
+
+  -- completion
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip'
+  use "saadparwaiz1/cmp_luasnip"
+  use "rafamadriz/friendly-snippets"
+
   use {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
